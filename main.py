7bb43e4d0b2cff28369d35992ee1d4e654bb26bb
barrozorcb <barrozorcb@gmail.com>
2025-08-15 11:45:46 -0300

    Update project to use env variable

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import requests
 import time
 import tweepy
-import config
+import os
+from dotenv import load_dotenv
 from datetime import datetime
 
+load_dotenv()
+
 def conectar_api():
-    client = tweepy.Client(access_token=config.access_token,
-                           access_token_secret=config.access_token_secret,
-                           consumer_key=config.consumer_api_key,
-                           consumer_secret=config.consumer_api_key_secret)
+    client = tweepy.Client(access_token=os.environ.get('ACCESS_TOKEN'),
+                           access_token_secret=os.environ.get('ACCESS_TOKEN_SECRET'),
+                           consumer_key=os.environ.get('CONSUMER_API_KEY'),
+                           consumer_secret=os.environ.get('CONSUMER_API_KEY_SECRET'))
     return client
 
-# Função para realizar a requisição à API
 def fazer_requisicao():
-    url = config.url_exchange
+    url = os.environ.get('URL_EXCHANGE')
     try:
         exchangeResponse = requests.get(url)
         if exchangeResponse.status_code == 200:
@@ -30,26 +32,24 @@ def create_tweet(exchangeResponse, client):
     client.create_tweet(text=texto, user_auth=True)
     print("Tweet enviado com sucesso!")
 
-# Função principal do bot
 def bot():
-    hora_inicio = config.init_hour  # Início: 09:00
-    hora_fim = config.end_hour  # Fim: 17:00
+    hora_inicio = int(os.environ.get('INIT_HOUR'))
+    hora_fim = int(os.environ.get('END_HOUR'))
     client = conectar_api()
     while True:
-        # Verificar se a hora atual está dentro do intervalo desejado
         hora_atual = datetime.now().hour
 
         if hora_inicio <= hora_atual < hora_fim:
-            responseExchange = fazer_requisicao()  # Realiza a requisição à API
-            if responseExchange and (float(responseExchange['EURBRL']['pctChange']) > config.max_variation or float(responseExchange['EURBRL']['pctChange']) < config.min_variation):
+            responseExchange = fazer_requisicao()
+            if responseExchange and (float(responseExchange['EURBRL']['pctChange']) > float(os.environ.get('MAX_VARIATION')) or float(responseExchange['EURBRL']['pctChange']) < float(os.environ.get('MIN_VARIATION'))):
                 create_tweet(responseExchange, client)
             else:
                 print("Não foi possível obter a cotação ou a variação foi menor que o desejado.")
                 print("Aguardando 1 minuto antes da próxima requisição...")
-            time.sleep(config.time_to_new_request)  # Espera 1 minuto (60 segundos)
+            time.sleep(int(os.environ.get('TIME_TO_NEW_REQUEST')))
         else:
             print("Fora do horário de operação. Esperando...")
-            time.sleep(config.time_to_new_request)  # Espera 1 minuto (60 segundos)
+            time.sleep(int(os.environ.get('TIME_TO_NEW_REQUEST')))
 
 if __name__ == "__main__":
     bot()
